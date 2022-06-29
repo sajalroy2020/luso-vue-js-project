4a16cfde8b25d9af00095941bcc4accbf0dd9b4d
sajalroy2020 <sajal2020roy@gmail.com>
2022-06-29 15:06:36 +0600

    luso-Vue-js

---
+++ src/views/AboutLuso.vue
@@ -0,0 +1,227 @@
+<template>
+    <div>
+        <!-- my profile start -->
+        <section class="">
+          <div class="">
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0"
+                  class=" slide-btn" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" class="slide-btn active"
+                  aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" class="slide-btn"
+                  aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item  layer">
+                  <img src="../assets/images/sea.jpg" class="d-block w-100 slide-img pathway-slide-image" alt="...">
+                  <div class="carousel-caption position-absolute">
+                    <div class="max-width placing">
+                      <div class="row align-items-center">
+                        <div class="col-md-5 text-center text-md-start">
+                          <h2 class="font-size-28 mb-0 letter-space text-uppercase">are you looking for</h2>
+                        </div>
+                        <div class="col-md-2 py-md-0 py-4">
+                          <img class="pathway-icon" src="../assets/images/img4.png" alt="">
+                        </div>
+                        <div class="col-md-5">
+                          <div class="">
+                            <h5 class=" font-size-28 fw-normal text-uppercase text-center text-md-end">a pathway to invest?</h5>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item active layer">
+                  <img src="../assets/images/sea.jpg" class="d-block w-100 slide-img pathway-slide-image" alt="...">
+                  <div class="carousel-caption position-absolute">
+                    <div class="max-width placing">
+                      <div class="row align-items-center">
+                        <div class="col-md-5 text-center text-md-start">
+                          <h2 class="font-size-28 mb-0 letter-space text-uppercase">are you looking for</h2>
+                        </div>
+                        <div class="col-md-2 py-md-0 py-4">
+                          <img class="pathway-icon" src="../assets/images/img4.png" alt="">
+                        </div>
+                        <div class="col-md-5">
+                          <div class="">
+                            <h5 class=" font-size-28 fw-normal text-uppercase text-center text-md-end">a pathway to invest?</h5>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item  layer">
+                  <img src="../assets/images/sea.jpg" class="d-block w-100 slide-img pathway-slide-image" alt="...">
+                  <div class="carousel-caption position-absolute">
+                    <div class="max-width placing">
+                      <div class="row align-items-center">
+                        <div class="col-md-5 text-center text-md-start">
+                          <h2 class="font-size-28 mb-0 letter-space text-uppercase">are you looking for</h2>
+                        </div>
+                        <div class="col-md-2 py-md-0 py-4">
+                          <img class="pathway-icon" src="../assets/images/img4.png" alt="">
+                        </div>
+                        <div class="col-md-5">
+                          <div class="">
+                            <h5 class=" font-size-28 fw-normal text-uppercase text-center text-md-end">a pathway to invest?</h5>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      <!-- my profile end -->
+
+      <!-- mission start -->
+      <section class="mission-section-space mission-space-t bg-white">
+        <div class="max-width">
+          <h2 class="text-center font-size-28 txt-2 mb-5 letter-space-3">OUR MISSION</h2>
+          <div class="row">
+            <div class="col-12 col-md-6">
+              <div class="txt-gray robo text-center text-md-end pe-md-2">
+                <p class="txt-gray pb-4 ">
+                  A<span class="fw-bold"> Luso Digital Assets</span> negoceia activos digigtais (criptomoeda) ao publico  pela internet
+                </p>
+                <p>
+                  Fazemos com que a experiencia do utilizador seja o mais simples e rapida possivel, para que novos investidores, curiousos, sem grande conhecimento tecnico possam entrar neste mercado
+                </p>
+              </div>
+            </div>
+            <div class="col-12 col-md-6">
+              <div class="txt-gray text-center robo text-md-start ps-md-2 mt-4 mt-md-5 pt-4">
+                <p class="pb-4">
+                  Temos tambem como objectivo a ajuda e educacao aos clientes desta area, de forma a que se sintam seguros a trabalhar connosco, e que tomem responsibilidade pelos seus fundos.
+                </p>
+                <p>
+                  Queremos ser a ponte dos activos mobiliarios para os activos digitais.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- mission end -->
+
+      <!-- bellow mission start -->
+      <section class="feature-section-space bg-1">
+        <div class="max-width">
+          <div class="row">
+            <div class="col-12 col-md-6">
+              <div class="pe-md-2">
+                <img class="w-100 minus-margin" src="../assets/images/mission-img.png" alt="">
+              </div>
+            </div>
+            <div class="col-12 col-md-6">
+              <div class="txt-4 text-center robo text-md-start ps-md-2 pt-5 pt-md-0">
+                <p class="font-size-18">What we know is a drop. What we don't know is an ocean, and while criptocurrency may look like the sea of opportunities you'll need a great navigator like luso Digital Assets alongside with you</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- bellow mission end -->
+
+      <!-- feature start -->
+      <section class="mission-section-space mission-space-b  bg-white">
+        <div class="max-width">
+          <div class="row flex-column-reverse flex-md-row">
+            <div class="col-12 col-md-6">
+              <div class="txt-gray text-start pe-md-2">
+                <p class="text-uppercase font-size-28 txt-2 mt-5 mt-md-0 letter-space-2">
+                  do you want<br class="d-none d-md-block"> to know to more<br class="d-none d-md-block"> about investing in<br class="d-none d-md-block"> criptocurrency with <br class="d-none d-md-block">luso digital assets 
+                </p>
+                  <ul class="mb-0 ps-0 pt-4 font-size-18 d-flex flex-wrap align-items-center">
+                    <li class="pe-3"><a class="underline txt-2 letter-space-2" href="javascript:;">Features</a></li>
+                    <li class="pe-3"><a class="underline txt-2 letter-space-2" href="javascript:;">Support</a></li>
+                    <li class="pe-3"><a class="underline txt-2 letter-space-2" href="javascript:;">Criptoguides</a></li>
+                  </ul>
+              </div>
+            </div>
+            <div class="col-12 col-md-6">
+              <div class="ps-md-2">
+                <img class="w-100 minus-margin2" src="../assets/images/img2.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- feature end -->
+    </div>
+</template>
+
+<script>
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { BarChart } from "echarts/charts";
+import {
+  GridComponent,
+  DatasetComponent,
+  LegendComponent,
+  TooltipComponent,
+} from "echarts/components";
+import VChart from "vue-echarts";
+use([
+  CanvasRenderer,
+  BarChart,
+  GridComponent,
+  DatasetComponent,
+  LegendComponent,
+  TooltipComponent,
+]);
+export default {
+  name: 'Welcome',
+  components: { VChart, },
+  data(){
+    return{
+        sliderBtn:'btn1',
+        option: {
+            color:['#1C01A5'],
+            legend: { },
+            tooltip: {  },
+            dataset: {
+            dimensions: ['MONTH', 'VALUE', ''],
+            source: [
+                { MONTH: 'FEB', VALUE: 20, },
+                { MONTH: 'MAR', VALUE: 100, },
+                { MONTH: 'APR', VALUE: 70, },
+                { MONTH: 'MAY', VALUE: 10, },
+                { MONTH: 'JUN', VALUE: 50, },
+                { MONTH: 'JUL', VALUE: 30, },
+                { MONTH: 'AUG', VALUE: 40, },
+                { MONTH: 'SEP', VALUE: 20, },
+                { MONTH: 'OCT', VALUE: 10, },
+                { MONTH: 'NOV', VALUE: 100, },
+                { MONTH: 'DEC', VALUE: 50, },
+            ]
+            },
+            xAxis: { type: 'category' },
+            yAxis: {  },
+            series: [{ type: 'bar' }, { type: 'bar' }],
+            animationDuration: 1500
+        },
+    }
+  },
+  methods:{
+     setSliderBtn(value){
+        this.sliderBtn = value;
+        if(this.sliderBtn === 'btn1'){
+            this.$router.push({ name: 'welcome'});
+        }
+        if(this.sliderBtn === 'btn2'){
+            this.$router.push({ name: 'home'});
+        }
+     }
+  },
+  mounted(){
+    
+  }
+}
+</script>
+   
